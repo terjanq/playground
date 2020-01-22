6693175144991dfc2e7e52bb06cbefb711bf4a09
terjanq <terjanq@users.noreply.github.com>
2020-01-22 20:19:57 +0100

    Update h4.js

--- h4.js
+++ h4.js
@@ -1,3 +1,3 @@
 var x = document.createElement('img');
-x.src=`admin.737663b4608e8689e2cc.d.zhack.ca`
+x.src=`https://admin.737663b4608e8689e2cc.d.zhack.ca`
 document.documentElement.appendChild(x);

